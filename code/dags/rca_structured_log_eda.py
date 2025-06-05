ceef3026148f68138d71487141a7e945dc7913e1
Sujit Tah <sujit.tah@gmail.com>
2025-06-05 14:23:42 +0530

    Added files

--- code/dags/rca_structured_log_eda.py
+++ code/dags/rca_structured_log_eda.py
@@ -2,13 +2,13 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta, timezone
 import boto3
-import matplotlib.pyplot as plt
 import pandas as pd
 import os
 from io import BytesIO
 import configuration
 from dask.distributed import Client
 import dask.dataframe as dd
+import matplotlib.pyplot as plt
 
 def perform_dask_eda_and_save_to_s3(**kwargs):
     client = Client("tcp://dask-scheduler.dask.svc.cluster.local:8786")
