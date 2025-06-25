29a76ce68e3c57edacd1dc854b1f83f1d4d79885
Sujit Tah <sujit.tah@gmail.com>
2025-06-25 13:28:54 +0530

    added new dag

---
+++ code/dags/dag_log_rca_orchestrator.py
@@ -0,0 +1,39 @@
+# orchestrator_dag.py
+from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="dag_log_rca_orchestrator",
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["orchestrator"]
+) as dag:
+
+    dag_log_parse = TriggerDagRunOperator(
+        task_id="parse_convert_raw_log_to_structured",
+        trigger_dag_id="dag_log_parse"
+    )
+
+    dag_log_template = TriggerDagRunOperator(
+        task_id="generate_template",
+        trigger_dag_id="dag_log_template"
+    )
+
+    dag_log_sequence = TriggerDagRunOperator(
+        task_id="generate_log_sequence",
+        trigger_dag_id="dag_log_sequence"
+    )
+
+    dag_log_clustering_kmeans = TriggerDagRunOperator(
+        task_id="train_rca_model_clustering_kmeans",
+        trigger_dag_id="dag_log_clustering_kmeans"
+    )
+
+    dag_log_deep_network_clustering_kmeans = TriggerDagRunOperator(
+        task_id="train_autoencoder_kmeans_pipeline",
+        trigger_dag_id="dag_log_deep_network_clustering_kmeans"
+    )
+
+    dag_log_parse >> dag_log_template >> dag_log_sequence >> dag_log_clustering_kmeans >> dag_log_deep_network_clustering_kmeans
