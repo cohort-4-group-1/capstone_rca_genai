abf8c8beb91ebf2684ed007bbcba73c3eb890a47
Sujit Tah <sujit.tah@gmail.com>
2025-06-25 20:44:00 +0530

    added dag

--- code/dags/dag_log_rca_orchestrator.py
+++ code/dags/dag_log_rca_orchestrator.py
@@ -26,6 +26,7 @@ with DAG(
         task_id="wait_for_template_completion",
         external_dag_id="dag_log_template",
         external_task_id="generate_template",
+        allowed_states=["success"],
         timeout=600,
         poke_interval=30,
         mode="poke"
@@ -42,9 +43,10 @@ with DAG(
     )
 
     dag_log_deep_network_clustering_kmeans = TriggerDagRunOperator(
-        task_id="train_rca_model_deep_network_clustering_kmeans",
+        task_id="train_autoencoder_kmeans_pipeline",
         trigger_dag_id="dag_log_deep_network_clustering_kmeans"
     )
 
-    dag_log_parse >> dag_log_template >> wait_for_template >> dag_log_sequence
+    dag_log_parse >> dag_log_template
+    dag_log_template >> wait_for_template >> dag_log_sequence
     dag_log_sequence >> [dag_log_clustering_kmeans, dag_log_deep_network_clustering_kmeans]
