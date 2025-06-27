9e1a662df4f6bd1f9669618425e347a84d08a859
dt233894 <ritam.banik@sscinc.com>
2025-06-27 13:38:42 -0400

    Removed S3KeySensor

--- code/dags/dag_log_parse.py
+++ code/dags/dag_log_parse.py
@@ -1,6 +1,5 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from datetime import datetime, timedelta, timezone
 from io import StringIO
 import pandas as pd
@@ -9,13 +8,9 @@ import re
 import configuration  
 
 def convert_raw_log_to_csv(**kwargs):
-    
-    s3_key = kwargs['task_instance'].xcom_pull(task_ids='wait_for_file')
-    print(f"Processing file: {s3_key}")
-    
-    #print(f"Reading file from S3: {configuration.SOURCE_BUCKET}/{configuration.RAW_FILE_KEY}")
+    print(f"Reading file from S3: {configuration.SOURCE_BUCKET}/{configuration.RAW_FILE_KEY}")
     s3 = boto3.client('s3', region_name=configuration.AWS_REGION)
-    obj = s3.get_object(Bucket=configuration.SOURCE_BUCKET, Key=s3_key)
+    obj = s3.get_object(Bucket=configuration.SOURCE_BUCKET, Key=configuration.RAW_FILE_KEY)
     
     raw_log = obj['Body'].read().decode('utf-8')
     print("Successfully read log file from S3")   
@@ -52,7 +47,6 @@ def upload_csv_to_silver_datalake(csv_data):
         Body=csv_data.encode('utf-8')
     )
     print(f"CSV uploaded to S3: {configuration.DEST_BUCKET}/{configuration.SILVER_FILE_KEY}")
-    
 
 # DAG Start Time (rounded down to nearest 30 mins minus 5 mins)
 now_utc = datetime.now(timezone.utc)
@@ -66,20 +60,8 @@ with DAG(
     is_paused_upon_creation=False,
     tags=['s3', 'validation', 'etl'],
 ) as dag:
-    
-    wait_for_file = S3KeySensor(
-        task_id='wait_for_file',
-        bucket_name={configuration.SOURCE_BUCKET},
-        bucket_key='raw/OpenStack_*.log',  # Wildcard pattern
-        wildcard_match=True,
-        timeout=300,  # 5 minutes timeout
-        poke_interval=30,  # Check every 30 seconds
-        aws_conn_id='aws_default'
-    )
-
     task = PythonOperator(
         task_id='parse_convert_raw_log_to_structured',
         python_callable=convert_raw_log_to_csv
     )
 
-    wait_for_file >> task
